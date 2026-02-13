4998dd48f5322564e034cc50cc8d06c00036f887
haruday7 <haru920707@gmail.com>
2026-02-13 18:00:44 +0900

    돌 놓기 구현(랜덤)

--- Assets/02.Scripts/Board/Board.cs
+++ Assets/02.Scripts/Board/Board.cs
@@ -1,8 +1,14 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Board : MonoBehaviour
 {
+    [SerializeField]
+    private Sprite whiteStone;
+    [SerializeField]
+    private Sprite blackStone;
+
     [SerializeField]
     private BoardGenerator boardGenerator;
 
@@ -11,9 +17,38 @@ public class Board : MonoBehaviour
     void Awake()
     {
         this.dicBlocks = this.boardGenerator.GenerateBoard();
-        
+    }
+    void Update()
+    {
+        if (Mouse.current != null && Mouse.current.leftButton.wasPressedThisFrame)
+        {
+            StoneOnClick();
+        }
     }
 
+    private void StoneOnClick()
+    {
+        Vector2 screenPosition = Mouse.current.position.ReadValue();
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(screenPosition);
+
+        RaycastHit2D hit = Physics2D.Raycast(mousePos, Vector2.zero);
+
+        if (hit.collider != null)
+        {
+            Block clickedBlock = hit.collider.GetComponent<Block>();
+
+            if (clickedBlock != null && !clickedBlock.HasStone)
+            {
+                //µ¹ »ý¼º
+                clickedBlock.SetStone(true, RandomStone());
+            }
+        }
+    }
+    public Sprite RandomStone()
+    {
+        Sprite randomSprite = Random.Range(0, 2) == 0 ? this.blackStone : this.whiteStone;
+        return randomSprite;
+    }
     //public void DicTest()
     //{
     //    foreach(KeyValuePair<(int, int), Block> pair in this.dicBlocks)

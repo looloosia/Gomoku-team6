976c77bcc87fee50e976b0e3d3a69397f97341fc
SiaKim <kimsia951@gmail.com>
2026-02-13 14:50:57 +0900

    AIState, BaseState, PlayerState, TurnManager 주석제거

--- Assets/02.Scripts/States/PlayerState.cs
+++ Assets/02.Scripts/States/PlayerState.cs
@@ -6,38 +6,38 @@ using UnityEngine;
 /// </summary>
 public class PlayerState : BaseState
 {
-    // private TurnManager turnManager;
-    //
-    // public PlayerState(bool isFirstPlayer)
-    // {
-    //     // _playerType = isFirstPlayer ? Constants.PlayerType.Player1 : Constants.PlayerType.Player2;
-    // }
-    //
-    // // 턴 변경
-    // public override void HandleNextTurn(GameLogic gameLogic)
-    // {
-    //     gameLogic.ChangeGameState();
-    // }
-    //
-    // public override void OnEnter(GameLogic gameLogic)
-    // {
-    //     // 상태 진입 시 로직 구현
-    //     gameLogic.blockController.onBlockClicked = (blockIndex) =>
-    //     {
-    //         // 블록이 클릭되었을 때 처리할 로직
-    //         HandleMove(gameLogic, blockIndex);
-    //     };
-    //
-    //     // 턴 표시 UI 업데이트
-    //     GameManager.Instance.SetGameTurn(_playerType);
-    // }
-    //
-    // public override void HandleMove(GameLogic gameLogic, int index)
-    // {
-    //     ProcessMove(gameLogic, index, _playerType);
-    // }
-    //
-    // public override void OnExit(GameLogic gameLogic)
-    // {
-    // }
+    private TurnManager turnManager;
+    
+    public PlayerState(bool isFirstPlayer)
+    {
+        // _playerType = isFirstPlayer ? Constants.PlayerType.Player1 : Constants.PlayerType.Player2;
+    }
+    
+    // 턴 변경
+    public override void HandleNextTurn(GameLogic gameLogic)
+    {
+        gameLogic.ChangeGameState();
+    }
+    
+    public override void OnEnter(GameLogic gameLogic)
+    {
+        // 상태 진입 시 로직 구현
+        gameLogic.blockController.onBlockClicked = (blockIndex) =>
+        {
+            // 블록이 클릭되었을 때 처리할 로직
+            HandleMove(gameLogic, blockIndex);
+        };
+    
+        // 턴 표시 UI 업데이트
+        GameManager.Instance.SetGameTurn(_playerType);
+    }
+    
+    public override void HandleMove(GameLogic gameLogic, int index)
+    {
+        ProcessMove(gameLogic, index, _playerType);
+    }
+    
+    public override void OnExit(GameLogic gameLogic)
+    {
+    }
 }
--- Assets/02.Scripts/States/TurnManager.cs
+++ Assets/02.Scripts/States/TurnManager.cs
@@ -2,58 +2,58 @@ using UnityEngine;
 
 /// <summary>
 /// GameType에 따른 턴 초기와 작업
-/// 
+/// GameLogic 역할 스크립트에서 blockcontroller, 보드 초기화 할 때 InitState() 호출해서 함께 초기화
 /// </summary>
 public class TurnManager : MonoBehaviour
 {
-    // public BaseState playerAState;
-    // public BaseState playerBState;
-    // private BaseState _currentState;
-    // private GameLogic  _gameLogic;
-    //
-    // public void InitState(Constants.GameType gameType, GameLogic gameLogic)
-    // {
-    //     _gameLogic = gameLogic;
-    //     switch (gameType)
-    //     {
-    //         case GameType.SinglePlay:
-    //             // 싱글 플레이어 모드 초기화 작업
-    //             playerAState = new PlayerState(true);
-    //             playerBState = new AIState(false);
-    //             
-    //             // 초기 상태 설정 (예: 플레이어 A부터 시작)
-    //             SetState(playerAState);
-    //             break;
-    //         case GameType.DualPlay:
-    //             // 듀얼 플레이어 모드 초기화 작업
-    //             playerAState = new PlayerState(true);
-    //             playerBState = new PlayerState(false);
-    //
-    //             // 초기 상태 설정 (예: 플레이어 A부터 시작)
-    //             SetState(playerAState);
-    //             break;
-    //     }
-    // }
-    //
-    // // 턴 변경
-    // public void ChangeGameState()
-    // {
-    //     if (_currentState == playerAState)
-    //     {
-    //         SetState(playerBState);
-    //     }
-    //     else
-    //     {
-    //         SetState(playerAState);
-    //     }
-    // }
-    //
-    //
-    // // 턴 바뀔 때 호출되는 메서드 (상태 전환 메서드)
-    // public void SetState(BaseState newState)
-    // {
-    //     _currentState?.OnExit(_gameLogic);
-    //     _currentState = newState;
-    //     _currentState.OnEnter((_gameLogic));
-    // }
+    public BaseState playerAState;
+    public BaseState playerBState;
+    private BaseState _currentState;
+    private GameLogic  _gameLogic;
+    
+    public void InitState(Constants.GameType gameType, GameLogic gameLogic)
+    {
+        _gameLogic = gameLogic;
+        switch (gameType)
+        {
+            case GameType.SinglePlay:
+                // 싱글 플레이어 모드 초기화 작업
+                playerAState = new PlayerState(true);
+                playerBState = new AIState(false);
+                
+                // 초기 상태 설정 (예: 플레이어 A부터 시작)
+                SetState(playerAState);
+                break;
+            case GameType.DualPlay:
+                // 듀얼 플레이어 모드 초기화 작업
+                playerAState = new PlayerState(true);
+                playerBState = new PlayerState(false);
+    
+                // 초기 상태 설정 (예: 플레이어 A부터 시작)
+                SetState(playerAState);
+                break;
+        }
+    }
+    
+    // 턴 변경
+    public void ChangeGameState()
+    {
+        if (_currentState == playerAState)
+        {
+            SetState(playerBState);
+        }
+        else
+        {
+            SetState(playerAState);
+        }
+    }
+    
+    
+    // 턴 바뀔 때 호출되는 메서드 (상태 전환 메서드)
+    public void SetState(BaseState newState)
+    {
+        _currentState?.OnExit(_gameLogic);
+        _currentState = newState;
+        _currentState.OnEnter((_gameLogic));
+    }
 }
